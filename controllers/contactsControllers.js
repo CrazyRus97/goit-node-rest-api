d0603db5e168ba44734e82b5f5246a0e86617df8
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2024-02-12 18:20:39 +0200

    hw2-1.0

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,62 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  getAll,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+  updateStatusContact,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import catchAsync from "../helpers/catchAsync.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = catchAsync(async (req, res) => {
+  const result = await getAll();
+  res.status(200).json(result);
+});
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = catchAsync(async (req, res) => {
+  const result = await getContactById(req.params.id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+});
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = catchAsync(async (req, res) => {
+  const result = await removeContact(req.params.id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+  return;
+});
 
-export const createContact = (req, res) => {};
+export const createContact = catchAsync(async (req, res) => {
+  const constact = await addContact(req.body);
+  res.status(201).json(constact);
+});
 
-export const updateContact = (req, res) => {};
+export const updateContactById = catchAsync(async (req, res) => {
+  const keys = Object.keys(req.body);
+  if (keys.length === 0) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+
+  const result = await updateContact(req.params.id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404);
+  }
+  return res.status(200).json(result);
+});
+
+export const updateFavorite = catchAsync(async (req, res) => {
+  const result = await updateStatusContact(req.params.id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+});
