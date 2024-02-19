b7dbf68012b787720809586bc60b85e45aaf571a
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2024-02-19 20:03:46 +0200

    hw3 finall1

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { app } from "./app.js";
 
-dotenv.config();
-
-const { DB_HOST, PORT } = process.env;
+dotenv.config({path: process.env.NODE_ENV === 'development'  ? './development.env' : './production.env'});
 
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Database connection successful");
     });
   })
